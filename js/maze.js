9682658866e8622933898b61025992e4f0d41878
dilan23Jill <muzicdilan@gmail.com>
2022-01-29 22:35:20 +0100

    fix

--- js/maze.js
+++ js/maze.js
@@ -29,7 +29,7 @@ function checkDiff() {
 }
 var info = document.querySelector(".info");
 var credits = document.querySelector(".credits");
-let genCom=1;
+let genCom = 1;
 var countDwn = document.getElementById("countDwnDiv");
 countDwn.style.display = "none";
 function startmaze() {
@@ -313,15 +313,14 @@ function startmaze() {
       }
     }
   }
- 
+
   var i = false;
   document.addEventListener("keydown", move);
-  
+
   document.addEventListener("keydown", startcountdown);
 
   function startcountdown() {
-    if(!generationComplete)
-    return;
+    if (!generationComplete) return;
     if (!i) {
       i = true;
       if (document.getElementById("easy").classList.contains("freeze")) {
@@ -412,8 +411,7 @@ function startmaze() {
   }
 
   function move(e) {
-    if(!generationComplete)
-    return;
+    if (!generationComplete) return;
     let key = e.key;
     let row = current.rowNum;
     let col = current.colNum;
@@ -438,7 +436,6 @@ function startmaze() {
             song1.pause();
 
             Swal.fire({
-
               title: "YOU WIN!",
               html: '<iframe width="450" height="300" src="./video/winning_SweetAlert.mp4" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>',
               confirmButtonColor: "#fa4366",
@@ -480,7 +477,6 @@ function startmaze() {
           }
         }
         break;
-
       case "ArrowLeft":
         if (!current.walls.leftWall) {
           let next = newMaze.grid[row][col - 1];
@@ -503,3 +499,71 @@ function startmaze() {
 close.addEventListener("click", () => {
   complete.style.display = "none";
 }); */
+var freezeShape1 = document.getElementById("easy");
+var freezeShape2 = document.getElementById("medium");
+var freezeShape3 = document.getElementById("hard");
+
+freezeShape1.addEventListener("click", function () {
+  freezeShape2.classList.remove("freeze");
+  freezeShape3.classList.remove("freeze");
+  this.classList.toggle("freeze");
+});
+
+freezeShape2.addEventListener("click", function () {
+  freezeShape1.classList.remove("freeze");
+  freezeShape3.classList.remove("freeze");
+  this.classList.toggle("freeze");
+});
+
+freezeShape3.addEventListener("click", function () {
+  freezeShape2.classList.remove("freeze");
+  freezeShape1.classList.remove("freeze");
+  this.classList.toggle("freeze");
+});
+var infoSound = new Audio("./audio/audio3.mp3");
+
+function showCredits() {
+  infoSound.play();
+  Swal.fire({
+    title: "INSTRUCTIONS!",
+    text: "ESCAPE THE FRONTMAN THROUGH LABYRINTH, BUT BE CAREFUL YOU DON'T HAVE A LOT OF TIME! GOOD LUCK",
+    confirmButtonColor: "#fa4366",
+    imageWidth: 515,
+    imageHeight: 300,
+    imageAlt: "SG_SWEETALERT",
+  });
+}
+function showInfo() {
+  infoSound.play();
+  Swal.fire({
+    title: "CREDITS!",
+    text: "MADE BY DILAN MUŽIČ",
+    confirmButtonColor: "#fa4366",
+    imageWidth: 515,
+    imageHeight: 300,
+    imageAlt: "SG_SWEETALERT",
+  });
+}
+var audioPlay = document.getElementById("playBtn");
+var audioStop = document.getElementById("playBtn2");
+var hidPlay = document.querySelector(".playAudio");
+var showPlay = document.querySelector(".playAudio2");
+var song1 = document.getElementById("sound");
+audioPlay.style.visibility = "hidden";
+hidPlay.style.visibility = "hidden";
+
+function playSound() {
+  audioPlay.style.visibility = "hidden";
+  hidPlay.style.visibility = "hidden";
+  audioStop.style.visibility = "visible";
+  showPlay.style.visibility = "visible";
+  song1.pause();
+}
+
+function stopSound() {
+  audioStop.style.visibility = "hidden";
+  showPlay.style.visibility = "hidden";
+  audioPlay.style.visibility = "visible";
+  hidPlay.style.visibility = "visible";
+  song1.play();
+}
